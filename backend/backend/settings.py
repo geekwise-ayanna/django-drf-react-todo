786a4cd02c42d8388d66772519ef36609c60e46c
Ayanna Bowers-Shaw <geekwise.ayanna.bs@gmail.com>
2019-12-03 19:14:59 +0000

    bugfix: removed CORS_ORIGIN_WHITELIST and replaced it with CORS_ORIGIN_ALLOW_ALL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,7 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# we whitelist localhost:3000 because that's where frontend will be served
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000/'
-)
+CORS_ORIGIN_ALLOW_ALL = True
